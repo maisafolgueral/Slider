f0f8e112d09b0393a5d85488e3c540c9b6cd6609
Maisa <seu e-mail>
2022-06-06 15:44:42 -0300

    Added initial slider

---
+++ index.js
@@ -0,0 +1,17 @@
+const previousEl = document.getElementById('previous');
+const nextEl = document.getElementById('next');
+const sliderEl = document.getElementById('slider');
+
+previousEl.addEventListener('click', onPreviousClick)
+nextEl.addEventListener('click', onNextClick)
+
+function onPreviousClick() {
+    const sliderWidth = sliderEl.offsetWidth;
+    sliderEl.scrollLeft -= sliderWidth;
+}
+
+function onNextClick() {
+    const sliderWidth = sliderEl.offsetWidth;
+    sliderEl.scrollLeft += sliderWidth;
+}
+
